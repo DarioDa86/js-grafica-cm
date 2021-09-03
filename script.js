cda455bf289e620c1117544f6574e1ad9f41b8e9
DarioDa86 <dario.dagnelli@gmail.com>
2021-09-03 18:43:05 +0200

    Completato esercizio, codice fixato

--- script.js
+++ script.js
@@ -3,17 +3,28 @@
 // Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
 
 // 1- Richiede all'utente un numero
-var numCell = prompt("inserisci un numero da 1 a 100");
+
+var numCell = document.getElementById("campo-utente");
+
+var btnInserisci = document.getElementById("btn-inserisci");
 
 // 2- creo il mio programma che inserisce il numero di quadrati indicati dall'utente
-for (var i = 1; i <= numCell; i++) {
-    document.getElementById("scacchiera").innerHTML += `<div class="quadrato">${i}</div>`;
-}
 
-// 3- Creo l'evento al "click" dell'utente, che colora il quadrato e fa uscire l'alert con il numero del quadrato selezionato
+btnInserisci.addEventListener("click",
+function dimensioniCampo(numCell){
+
+    var numCell = document.getElementById("campo-utente").value;
+
+    for (var i = 1; i <= numCell; i++) {
+        document.getElementById("scacchiera").innerHTML += `<div class="quadrato">${i}</div>`;
+    }
+}
+);
+
+// 3- Creo l'evento al "click" dell'utente, che colora il quadrato al click e fa uscire l'alert con il numero del quadrato selezionato
 document.getElementById("scacchiera").addEventListener("click",
     function(event) {
         event.target.classList.toggle("cliccato");
         alert(event.target.innerHTML);
     }
-);
+)
